484a4673c47477d6e51286439b6cf2ab050f1c3a
J-oseph <>
2023-12-27 17:51:22 -0800

    move definitions to file

--- core/alu.v
+++ core/alu.v
@@ -1,15 +1,5 @@
 
-`define OP_R    1'b0
-`define OP_B    1'b1
-
-`define R_ADD   2'b00
-`define R_AND   2'b01
-`define R_OR    2'b10
-`define R_XOR   2'b11
-
-`define B_BEQ   1'b0
-`define B_BLT   1'b1
-
+`include "core/definitions.v"
 
 module Alu #(
     parameter DW = 8,
---
+++ core/definitions.v
@@ -0,0 +1,17 @@
+
+`define OP_R    1'b0
+`define OP_B    1'b1
+
+`define R_ADD   2'b00
+`define R_AND   2'b01
+`define R_OR    2'b10
+`define R_XOR   2'b11
+
+`define B_BEQ   1'b0
+`define B_BLT   1'b1
+
+
+`define REG0    2'b00
+`define REG1    2'b01
+`define REG2    2'b10
+`define REG3    2'b11
