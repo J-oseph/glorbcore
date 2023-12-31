adc2091985bc18824a96da0a9ed511520515376d
J-oseph <>
2023-12-30 16:13:43 -0800

    R and I type decoding

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,7 +1,34 @@
-import argparse
+import argparse, enum, os
+
+
+class Opcode(enum.Enum):
+    R = 0b00
+    I = 0b01
+    B = 0b10
+    M = 0b11
+
+class RFunct(enum.Enum):
+    ADD = 0b000
+    AND = 0b001
+    OR = 0b010
+    XOR = 0b011
+
+class IFunct(enum.Enum):
+    ADDI = 0b0
+
+class BFunct(enum.Enum):
+    BEQ = 0b00
+    BLT = 0b01
+
+class Register(enum.Enum):
+    X0 = 0b00
+    X1 = 0b01
+    X2 = 0b10
+    X3 = 0b11
 
 
 def main():
+    #''' 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i", "--input_file",
@@ -16,8 +43,42 @@ def main():
         help="File to be outputted.",
     )
     args = parser.parse_args()
-    print(args.input_file)
-    print(args.output_file)
+    input_file = args.input_file
+    assert (os.path.exists(input_file))
+    #'''
+    commands = []
+    with open(input_file, 'r') as f: lines = f.readlines()
+    lines = [line.strip() for line in lines]
+    for line in lines:
+        keywords = line.split(' ')
+        keywords = [word.upper().replace(',', '') for word in keywords]
+        assert len(keywords) > 0
+        funct = keywords[0]
+        if any(member.name == funct for member in RFunct):
+            # R type
+            assert len(keywords) == 4
+            rd  = Register[keywords[1]]
+            rs1 = Register[keywords[2]]
+            rs2 = Register[keywords[3]]
+
+            commands.append(f"{RFunct[funct].value:04b}{rs2.value:02b}{rs1.value:02b}{rd.value:02b}{Opcode['R'].value:02b}")
+        elif any(member.name == funct for member in IFunct):
+            # I type
+            assert len(keywords) == 4
+            rd  = Register[keywords[1]]
+            rs1 = Register[keywords[2]]
+            imm = int(keywords[3])
+            assert -2**(6-1) <= imm <= (2**(6-1)) - 1
+
+            commands.append(f"{imm:06b}{rs1.value:02b}{rd.value:02b}{Opcode['I'].value:02b}")
+
+        else:
+            assert(False), 'something went wrong!'
+
+        for com in commands:
+            assert len(com) == 12
+        print(commands)
+
 
 
 if __name__ == '__main__':
